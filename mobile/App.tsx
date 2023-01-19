1de70e6aa0fe18ddbe3e802f22529006b4f9a85b
Emanoel Antonio <dev.emanoel@protonmail.com>
2023-01-19 19:42:06 -0300

    :sparkles: feat: Home page and base functions

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,45 +2,34 @@ import {
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
-  Inter_800ExtraBold, useFonts
-} from '@expo-google-fonts/inter';
+  Inter_800ExtraBold,
+  useFonts
+} from "@expo-google-fonts/inter";
 
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
-import { Loading } from './src/components/Loading';
- 
+import { StatusBar } from "react-native";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
-  
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_800ExtraBold
-  })
-  
+    Inter_800ExtraBold,
+  });
+
   if (!fontsLoaded) {
-    return (
-      <Loading/>
-    )
+    return <Loading />;
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Habits App!</Text>
-      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
-    </View>
+    <>
+      <Home />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
