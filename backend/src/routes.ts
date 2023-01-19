d22a8239d9233286e4bd8d1ea884c1b5f8228768
Emanoel Antonio <dev.emanoel@protonmail.com>
2023-01-19 10:40:24 -0300

    :sparkles: Routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 import { prisma } from './lib/prisma';
 
 
-export async function appRoutes(app:FastifyInstance) {
+export async function appRoutes(app: FastifyInstance) {
   app.post('/habits', async (request) => {
     const createHabitBody = z.object({
       title: z.string(),
@@ -26,5 +26,45 @@ export async function appRoutes(app:FastifyInstance) {
         }
       }
     });
+  });
+
+  app.get('/day', async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    });
+
+    const { date } = getDayParams.parse(request.query);
+    const parseDate = dayjs(date).startOf('day');
+    const weekDay = parseDate.get('day');
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          }
+        }
+      }
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parseDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      }
+    });
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
   });
 }
