341623efbc5ba4b93791dc7f31b3023bb5f210de
Emanoel Antonio <dev.emanoel@protonmail.com>
2023-02-14 16:46:24 -0300

    :sparkles: feat: Habit page empty

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -32,7 +32,7 @@ export function New() {
   async function handleCreateNewHabit() {
     try {
       if (!title.trim() || weekDays.length === 0) {
-        Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha a recorrência!')
+        return Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha a recorrência!')
       }
 
       await api.post('/habits', { title, weekDays });
